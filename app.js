60db0794bc0843cf254a8b35871e2cf587ce83fe
dao vo <gary.vo32@gmail.com>
2022-06-26 18:35:17 -0700

    restarted proj with express generator using ejs for view

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-const express = require("express")
-const path = require("path");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const mongoDb = "YOUR MONGO URL HERE";
-mongoose.conneect(mongoDb, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
 
-const User = mongoose.model(
-    "User",
-    new Schema({
-        username: { type: String, require: true},
-        password: { type: String, required: true}
-    })
-);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-const app = express();
-app.set("views", __dirname);
-app.set("view engine", "ejs");
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.urlencoded({extended: false}));
-
-app.get("/", (req, res) => res.render("index"));
-
-app.listen(3000, () => console.log("app listening on port 3000!"))
+module.exports = app;
